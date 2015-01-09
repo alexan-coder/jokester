14963925c55a8d0b9aac9bc84522ca2431802440
Alexander Biles <alexander.c.biles@gmail.com>
2015-01-09 10:28:12 -0500

    created joke model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150109151240) do
+
+  create_table "jokes", force: :cascade do |t|
+    t.integer "user_id"
+    t.text    "body"
+    t.string  "title"
+  end
 
 end
